17f35b5f876c8fb51f69d83e4bc453f0477f5d0d
jihee <esder1310@naver.com>
2024-05-03 21:49:35 +0900

    setting:react랑 라이브러리 설치 / global css 설정 중..props 오류남

---
+++ cd-gq/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import theme from './styles/theme';
+import Main from './pages/main';
+
+function App() {
+  return (
+    <>
+      <div>
+        hi!
+        <Main />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ cd-gq/src/styles/theme.js
@@ -0,0 +1,55 @@
+const theme={
+    colors:{
+        mainPageColor:'#2ADEA1',
+        white:'#fff',
+        darkblue:'#14264C',
+        lightblue:'#E2EAFA',
+        red:'#FF4A4A',
+        orange:'#FF6A4A',
+        yellow:'#FFAC4A',
+        lightorange:'#FFDAD2',
+        green:'#2ADEA1',
+        skyblue:'#2CE3E6',
+    },
+    fonts:{
+        fontFamily:'"Noto Sans KR", sans-serif',
+        slogan:{
+            korean:'20px',
+            english:'36px'
+        },
+        placeHolder:'20px',
+        button:'28px',
+        select:'24px',
+        day:{
+            month:'18px',
+            day:'16px'
+        },
+        component:{
+            percent:'42px',
+            state:'28px',
+            name:'32px'
+        },
+        weather:'18px',
+    },
+    icons:{
+        color:'#fff',
+        //아이콘 사이즈
+        componentState:'150px',
+        componentCircleArrow:'22px',
+        weather:'40px',
+        downFill:'24px',
+        upFill:'24px',
+        arrowRightCircle:'28px',
+        location:'24px',
+        crop:'24px',
+        //동그라미 느낌표 아이콘
+        exclamation:'24px',
+        alert:'24px',
+        componentLeft:'24px',
+        componentRight:'24px',
+        home:'30px',
+        backArrow:'34px'
+    }
+}
+
+export default theme;
