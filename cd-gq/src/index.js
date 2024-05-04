66e67297b5e345a90ae6c2f227a7358b9efadcd3
wiseah <hyunah2765@gmail.com>
2024-05-05 05:01:08 +0900

    Revert "Revert "Merge branch 'main' into hyunah1""
    This reverts commit eab47a5c57a6ad2e70d7fa80ffff147cc91e3c0f.

--- cd-gq/src/index.js
+++ cd-gq/src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from 'styled-components';
+import { createRoot } from 'react-dom/client';
 import GlobalStyle from './styles/global';
+import theme from './styles/theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const rootElement=document.getElementById('root');
+
+createRoot(rootElement).render(
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
-);
-
+  </ThemeProvider>
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
