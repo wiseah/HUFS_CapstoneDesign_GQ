a5a91cc1ba63d955c2ef53687e67fd442ed12507
wiseah <hyunah2765@gmail.com>
2024-05-04 16:56:20 +0900

    test: 테스트입니다

---
+++ cd-gq/src/pages/Test1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test1() {
+  return (
+    <div>
+      테스트페이지입니다.
+    </div>
+  )
+}
+
+export default Test1
--- cd-gq/src/routes.js
+++ cd-gq/src/routes.js
@@ -2,6 +2,7 @@ import Main from "./pages/main";
 import Sitemap from "./pages/Sitemap";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Test1 from "./pages/Test1";
 
 const routes = [
     {
@@ -22,6 +23,11 @@ const routes = [
         path: '/detail',
         element: <Detail />,
         name: '해충설명페이지(세 번째 페이지)'
+    },
+    {
+        path: '/test',
+        element: <Test1 />,
+        name: '테스트페이지'
     },
 ]
 
