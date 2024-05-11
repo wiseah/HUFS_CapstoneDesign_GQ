0f1a9b6855b2a42a6e1f7d0c3df99515aa697434
wiseah <hyunah2765@gmail.com>
2024-05-11 16:22:05 +0900

    feat: Home.js 페이지 백그라운드 설정 완료

--- cd-gq/src/pages/Home.js
+++ cd-gq/src/pages/Home.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components';
 import Header from '../components/Header';
+import State from '../components/State'
+import WormRightTopImg from '../assets/images/wormRightTop.png';
+import CloudImg from '../assets/images/cloud.png'
+import Weather from '../components/Weather';
 
 const Background = styled.div`
   display: flex;
@@ -11,6 +15,12 @@ const Background = styled.div`
   .scroll::-webkit-scrollbar {
     display: none;
   }
+
+  .header{
+    background-color: white;
+  }
+    
+  
 `;
 
 const Button = styled.button`
@@ -20,7 +30,35 @@ background-color: #3EB7EC;
 color: #ffffff;
 font-size: 24px;
 font-weight: 500;
-margin-top: 400px;
+margin-top: 10px;
+`;
+
+const Container = styled.div`
+position: relative;
+margin: 0 30px 30px 30px;
+`
+const Line=styled.div`
+  width:360px;
+  height:3px;
+
+  border-radius:15px;
+
+  background-color:#665E5E;
+`;
+
+const WormRightTop = styled.img`
+
+  position: absolute; //애벌레 위치 움직이지 않으려면 부모에 relative, 자식 속성에 absolute주고 위치 조정해야함.
+  left: 254px;
+  top: -32px;
+  z-index: 1;
+`;
+
+const CloudImage = styled.img`
+position: absolute;
+left: 254px;
+top: -32px;
+z-index: 1;
 `;
 
 function Home() {
@@ -28,8 +66,19 @@ function Home() {
 
   return (
     <Background>
-      <Header/>
+      <Header className='header'/>
+      <Container>
+        <WormRightTop src={WormRightTopImg}/>  
+        <State/> 
+      </Container>
+      <Line/>
+      <Container>
+        <CloudImage src={CloudImg}/>
+        <Weather/>
+      </Container>
+      
       <Button onClick={() => navigate('/')}>사이트맵으로 가기</Button>
+      
     </Background>
   )
 }
