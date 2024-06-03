3b84aac9d63b202c7c5c48f590464953130eddb1
jihee <esder1310@naver.com>
2024-06-04 05:05:44 +0900

    feat(API)
    props 설정 후 api연동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components';
 
@@ -9,6 +9,7 @@ import Weather from '../components/HomePage/Weather';
 import WormRightTopImg from '../assets/images/wormRightTop.png';
 import CloudImg from '../assets/images/cloud.png'
 import Weather1 from '../components/HomePage/Weather copy';
+import getStateInfo from '../APIs/get/getStateInfo';
 
 const Background = styled.div`
   display: flex;
@@ -62,12 +63,63 @@ function Home() {
   const navigate=useNavigate();
   const [selectedCrop, setSelectedCrop] = useState('콩');
 
+  const [infoResult, setInfoResult] = useState({
+      "selected_crop": selectedCrop,
+      "selected_location": "충청도",
+      "selected_date": "2024-06-04",
+      "percentages": {
+          "담배거세미나방": 50
+      },
+      "riskLevels": {
+          "담배거세미나방": "주의"
+      }
+  });
+  const [stateData, setStateData] = useState({
+    "percent":"50",
+    "pestName":"톱다리개미허리노린재"
+  });
+  const [nowData, setNowData] = useState(0);
+  
+  const bugs = [
+    "담배거세미나방",
+    "미국선녀벌레",
+    "썩덩나무노린재",
+    "작은뿌리파리",
+    "톱다리개미허리노린재",
+    "흰점도둑나방"
+  ]
+
+  useEffect( () => {
+    const fetchStateInfo = async () => {
+      try{
+        const result = await getStateInfo();
+        setInfoResult(result);
+        console.log("state 데이터:", result);
+
+        setStateData({
+          "percent":infoResult.percentages[bugs[nowData]],
+          "pestName":bugs[nowData]
+        })
+      } catch (error) {
+        console.log("앨범 정보를 가져오는 데 실패했습니다:", error);
+      }
+    };
+    fetchStateInfo();
+  }, [])
+
+  useEffect(()=>{
+    setStateData({
+      "percent":infoResult.percentages[bugs[nowData]],
+      "pestName":bugs[nowData]
+    })
+  }, [nowData])
+
   return (
     <Background>
       <Header selectedCrop={selectedCrop} setSelectedCrop={setSelectedCrop}/>
       <Container>
         <WormRightTop src={WormRightTopImg}/>  
-        <State selectedCrop={selectedCrop}/> 
+        <State selectedCrop={stateData} nowData={nowData} setNowData={setNowData}/> 
       </Container>
       <Line/>
       <Container>
