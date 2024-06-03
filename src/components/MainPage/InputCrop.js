4804e81305e3648cbc0b730075b7608085dc1a81
jihee <esder1310@naver.com>
2024-06-04 01:35:59 +0900

    feat(InputCrop)
    작물 선택시 1개만 선택되고 active 상태 유지하게 기능 구현

--- src/components/MainPage/InputCrop.js
+++ src/components/MainPage/InputCrop.js
@@ -1,240 +1,211 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { BsCaretDownFill } from 'react-icons/bs';
 import { BsCaretUpFill } from 'react-icons/bs';
 import { BsGeoFill } from 'react-icons/bs';
-import {BsQuestionCircleFill} from 'react-icons/bs';
 
-const InputDiv=styled.button`
-  display:flex;
-  flex-direction:row;
-  align-items:center;
+const InputDiv = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   text-align: center;
 
-  cursor:pointer;
+  cursor: pointer;
 
-  padding:19px 30px;
+  padding: 19px 30px;
 
-  width:${({theme})=>theme.divSizes.mainInputDivWidth};
-  height:${({theme})=>theme.divSizes.mainInputDivHeight};
+  width: ${({ theme }) => theme.divSizes.mainInputDivWidth};
+  height: ${({ theme }) => theme.divSizes.mainInputDivHeight};
 
-  border : none;
-  border-radius:${({theme})=>theme.borderRadius.input};
-  
-  color:${({theme})=>theme.colors.darkblue};
-  background-color:${({theme})=>theme.colors.lightgreen};
+  border: none;
+  border-radius: ${({ theme }) => theme.borderRadius.input};
+
+  color: ${({ theme }) => theme.colors.darkblue};
+  background-color: ${({ theme }) => theme.colors.lightgreen};
   transition: background-color 0.2s;
-  
-  font-size:${({theme})=>theme.fontSizes.input};
-  font-weight:${({theme})=>theme.fontsWeights.inputLight};
 
-  .icon{
-    width:${({theme})=>theme.icons.downFill};
-    height:${({theme})=>theme.icons.downFill};
+  font-size: ${({ theme }) => theme.fontSizes.input};
+  font-weight: ${({ theme }) => theme.fontsWeights.inputLight};
 
-    margin:auto 0px auto auto;
-    color:${({theme})=>theme.colors.darkblue};
+  .icon {
+    width: ${({ theme }) => theme.icons.downFill};
+    height: ${({ theme }) => theme.icons.downFill};
+
+    margin: auto 0px auto auto;
+    color: ${({ theme }) => theme.colors.darkblue};
   }
 
   &:hover {
-    color : ${({theme})=>theme.colors.darkblue} !important;
+    color: ${({ theme }) => theme.colors.darkblue} !important;
     transition: color 0.2s !important;
-    background-color:${({theme})=>theme.hoverColors.main} !important;
+    background-color: ${({ theme }) => theme.hoverColors.main} !important;
     transition: background-color 0.2s !important;
-    
-    font-weight:${({theme})=>theme.fontsWeights.inputBold} !important;
 
+    font-weight: ${({ theme }) => theme.fontsWeights.inputBold} !important;
   }
 
-  button{
-    width:100%;
-    height:100%;
-  }
-`
-
-const IsToggledDiv=styled.div`
-  display:flex;
-  flex-direction:column;
-
-  text-align:center;
-  align-items:center;
-
-  width:364px;
-  height:243px;
-
-  color:${({theme})=>theme.colors.darkblue};
-  background-color:${({theme})=>theme.colors.white};
-  box-shadow:0px 3px 5px #0000002f;
-
-  border-radius:15px;
-
-  font-weight:${({theme})=>theme.fontsWeights.inputBold};
-
-  .toggleHr{
-    width:321px;
-    height:3px;
-
-    border-radius:50%;
-
-    background-color:${({theme})=>theme.colors.darkblue};
+  button {
+    width: 100%;
+    height: 100%;
   }
 `;
 
-const IsToggledHeader=styled.div`
-  display:flex;
-  flex-direction:row;
+const IsToggledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
 
-  margin-top:20px;
-  margin-left:0px;
+  text-align: center;
+  align-items: center;
 
-  text-align:left;
+  width: 364px;
+  height: 243px;
 
-  font-size:${({theme})=>theme.fontSizes.inputBold};
-  font-weight:${({theme})=>theme.fontsWeights.inputBold};
+  color: ${({ theme }) => theme.colors.darkblue};
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 3px 5px #0000002f;
 
-  span{
-    margin:auto 190px 17px 11px;
+  border-radius: 15px;
+
+  font-weight: ${({ theme }) => theme.fontsWeights.inputBold};
+
+  .toggleHr {
+    width: 321px;
+    height: 3px;
+
+    border-radius: 50%;
+
+    background-color: ${({ theme }) => theme.colors.darkblue};
   }
 `;
 
-const IsToggledLine=styled.div`
-  width:321px;
-  height:3px;
+const IsToggledHeader = styled.div`
+  display: flex;
+  flex-direction: row;
 
-  border-radius:15%;
+  margin-top: 20px;
+  margin-left: 0px;
 
-  background-color:${({theme})=>theme.colors.darkblue};
-`;
-const IsToggledButtonsDiv=styled.div`
-  display:flex;
-  flex-direction:row;
-`;
-const IsToggleCropDiv=styled.div`
-  display:flex;
-  text-align:center;
-  align-items:center;
-  justify-content:center;
+  text-align: left;
 
-  width:100px;
-  height:40px;
+  font-size: ${({ theme }) => theme.fontSizes.inputBold};
+  font-weight: ${({ theme }) => theme.fontsWeights.inputBold};
 
-  margin-top:23px;
-
-  color:${({theme})=>theme.colors.darkblue};
-  background-color:${({theme})=>theme.colors.white};
-  border-radius:25px;
-  
-  
-  font-size:${({theme})=>theme.fontSizes.inputSelect};
-  font-weight:${({theme})=>theme.fontsWeights.inputSelect};
-  
-  &:hover{
-    color:${({theme})=>theme.colors.white};
-    transition:color 0.2s;
-    
-    background-color:${({theme})=>theme.colors.darkblue};
-    transition:background-color 0.2s;
+  span {
+    margin: auto 190px 17px 11px;
   }
 `;
 
-function InputCrop( {onToggle, onClick} ) {
-  const [isToggled, setIsToggled]=useState(false);
+const IsToggledLine = styled.div`
+  width: 321px;
+  height: 3px;
 
-  const handleToggle=()=>{
-    setIsToggled(prevState=>!prevState);
-    onToggle(!isToggled);  // 토글 상태에 따라 Main에 알림
-  };
-  
-  const [selectCrop, setSelectCrop]=useState(false);
+  border-radius: 15%;
 
-  const [selectTangerine,setSelectTangerine]=useState(false);
-  const [selectPapper,setSelectPepper]=useState(false);
-  const [selectBean,setSelectBean]=useState(false);
-  const [selectStrawberry,setSelectStrawberry]=useState(false);
-  const [selectApple,setSelectApple]=useState(false);
+  background-color: ${({ theme }) => theme.colors.darkblue};
+`;
+const IsToggledButtonsDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const IsToggleCropDiv = styled.div`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
 
-  const handleCrop=()=>{
-    setSelectCrop(!selectCrop);
+  width: 100px;
+  height: 40px;
+
+  margin-top: 23px;
+
+  color: ${({ theme }) => theme.colors.darkblue};
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 25px;
+
+  font-size: ${({ theme }) => theme.fontSizes.inputSelect};
+  font-weight: ${({ theme }) => theme.fontsWeights.inputSelect};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+    transition: color 0.2s;
+
+    background-color: ${({ theme }) => theme.colors.darkblue};
+    transition: background-color 0.2s;
+  }
+
+  &.active {
+    color: white;
+    background-color: ${({ theme }) => theme.colors.darkblue};
+  }
+`;
+
+function InputCrop({ onToggle, onClick }) {
+  const [isToggled, setIsToggled] = useState(false);
+  const [selectedCrop, setSelectedCrop] = useState(null);
+
+  const handleToggle = () => {
+    setIsToggled((prevState) => !prevState);
+    onToggle(!isToggled); // 토글 상태에 따라 Main에 알림
   };
-  const handleTangerin=()=>{
-    if(setSelectCrop===true){
-      setSelectTangerine(true);
-    }else{
-      setSelectTangerine(false);
-    }
-  };
-  const handlePapper=()=>{
-    if(setSelectCrop===true){
-      setSelectPepper(true);
-    }else{
-      setSelectPepper(false);
-    }
-  };
-  const handleBean=()=>{
-    if(setSelectCrop===true){
-      setSelectBean(true);
-    }else{
-      setSelectBean(false);
-    }
-  };
-  const handleApple=()=>{
-    if(setSelectCrop===true){
-      setSelectApple(true);
-    }else{
-      setSelectApple(false);
-    }
+
+  const handleCropClick = (crop) => {
+    setSelectedCrop(crop);
+    onClick(crop); // 작물 선택 시 부모 컴포넌트에 알림
   };
 
   return (
     <>
       <InputDiv onClick={handleToggle}>
         작물선택
-        <button className='icon'>
-          {isToggled?<BsCaretUpFill className='icon'/>:<BsCaretDownFill className='icon'/>}
+        <button className="icon">
+          {isToggled ? <BsCaretUpFill className="icon" /> : <BsCaretDownFill className="icon" />}
         </button>
       </InputDiv>
       {isToggled && (
-            <IsToggledDiv>
-              <IsToggledHeader>
-                <BsGeoFill /> <span>지역 선택</span>
-              </IsToggledHeader>
-              <IsToggledLine />
-              <IsToggledButtonsDiv onClick={handleCrop}>
-                {/* <IsToggleCropDiv
-                  onClick={()=>onClick('감귤')}
-                  style={selectTangerine && {backgroundColor:'#14264C', color:'white'}}
-                >
-                  감귤
-                  </IsToggleCropDiv> */}
-                <IsToggleCropDiv
-                    onClick={() => onClick('감귤')}
-                    style={selectTangerine ? { backgroundColor: '#14264C', color: 'white' } : null}
-                >
-                  감귤
-                </IsToggleCropDiv>
-
-                <IsToggleCropDiv
-                  onClick={()=>onClick('고추')}
-                >고추</IsToggleCropDiv>
-                <IsToggleCropDiv
-                  onClick={()=>onClick('콩')}
-                  // style={{ backgroundColor: selectCrop === '콩' ? 'white' : '#14264c' }}
-                >콩</IsToggleCropDiv>
-              </IsToggledButtonsDiv>  
-              <IsToggledButtonsDiv>
-                <IsToggleCropDiv
-                  onClick={()=>onClick('딸기')}
-                >딸기</IsToggleCropDiv>
-                <IsToggleCropDiv
-                  onClick={()=>onClick('사과')}
-                >사과</IsToggleCropDiv>
-              </IsToggledButtonsDiv>
-            </IsToggledDiv>
-        )}
-        {!isToggled && (<></>)}
+        <IsToggledDiv>
+          <IsToggledHeader>
+            <BsGeoFill /> <span>지역 선택</span>
+          </IsToggledHeader>
+          <IsToggledLine />
+          <IsToggledButtonsDiv>
+            <IsToggleCropDiv
+              onClick={() => handleCropClick('감귤')}
+              className={selectedCrop === '감귤' ? 'active' : ''}
+            >
+              감귤
+            </IsToggleCropDiv>
+            <IsToggleCropDiv
+              onClick={() => handleCropClick('고추')}
+              className={selectedCrop === '고추' ? 'active' : ''}
+            >
+              고추
+            </IsToggleCropDiv>
+            <IsToggleCropDiv
+              onClick={() => handleCropClick('콩')}
+              className={selectedCrop === '콩' ? 'active' : ''}
+            >
+              콩
+            </IsToggleCropDiv>
+          </IsToggledButtonsDiv>
+          <IsToggledButtonsDiv>
+            <IsToggleCropDiv
+              onClick={() => handleCropClick('딸기')}
+              className={selectedCrop === '딸기' ? 'active' : ''}
+            >
+              딸기
+            </IsToggleCropDiv>
+            <IsToggleCropDiv
+              onClick={() => handleCropClick('사과')}
+              className={selectedCrop === '사과' ? 'active' : ''}
+            >
+              사과
+            </IsToggleCropDiv>
+          </IsToggledButtonsDiv>
+        </IsToggledDiv>
+      )}
     </>
-  )
+  );
 }
 
-export default InputCrop
+export default InputCrop;
