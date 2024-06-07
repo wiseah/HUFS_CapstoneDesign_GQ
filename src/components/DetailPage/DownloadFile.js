a00b9941567228971483985cd3c12f526ede939d
jihee <esder1310@naver.com>
2024-06-08 00:37:37 +0900

    docs(DownloadFile.js)
    : 기능 구현 중 에러나서 버림. 다음 pr시 삭제하기

--- src/components/DetailPage/DownloadFile.js
+++ src/components/DetailPage/DownloadFile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import FileDb from '../../db/file.json';
 import {BiArchiveIn} from 'react-icons/bi';
 
 const ToggleContentDownload=styled.button`
@@ -28,20 +27,21 @@ const ToggleContentDownload=styled.button`
 
 `;
 
-const DownloadFile = (name,fileName) => {
+const DownloadFile = (name,fileUrl) => {
   const handleDownload = () => {
-    const fileId = '1c9wuNH8a57PQ7q64l-WOUI97994Q3vnldCrRuEEgn5k';
-    const downloadUrl = 'https://docs.google.com/spreadsheets/d/1gVxxbRwWqi2LIn2dzbyVKeTO4cZZu0U6/export?format=xlsx'
 
-    const pestName=`${FileDb.pestName}`;
-    const FILE_URL=`${FileDb.url}`;
+    const PestName=`${name}`
+    const FILE_URL=`${fileUrl}`;
 
     // 링크를 동적으로 생성하여 클릭 이벤트를 트리거하여 파일을 다운로드
     const link = document.createElement('a');
+    console.log('name : ',PestName);
+    console.log('link : ',link);
+    console.log('file url : ',FILE_URL);
     
     // link.href = downloadUrl;
     link.href=FILE_URL;
-    link.setAttribute('download', '');  // filename을 지정하지 않으면 구글 드라이브의 원본 파일명을 사용합니다
+    link.setAttribute('download', `${PestName}`);  // filename을 지정하지 않으면 구글 드라이브의 원본 파일명을 사용합니다
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
