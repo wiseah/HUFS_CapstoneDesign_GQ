0f7ca4c4b002deb4d459ee41e677807bacd0deda
wiseah <hyunah2765@gmail.com>
2024-06-10 02:48:23 +0000

    docs: index.html 에서 https로 바뀌는 부분 수정

--- src/APIs/get/getStateInfo.js
+++ src/APIs/get/getStateInfo.js
@@ -1,26 +1,13 @@
-// import axiosInstance from "../axiosinstance";
+import axiosInstance from "../axiosinstance";
 
-// async function getStateInfo(){
-//     try{
-//         const response = await axiosInstance.get(
-//             `/weather/api/get/maininfo/`,{}
-//         )
-//         return response.data;
-//     } catch (error) {
-//         console.log('오류 발생', error);
-//     }
-// }
-// export default getStateInfo;
-
-import axios from 'axios';
-
-const getStateInfo = async () => {
-  try {
-    const response = await axios.get('/api/weather/api/get/maininfo/');
-    return response.data;
-  } catch (error) {
-    throw new Error('Error fetching state info:', error);
-  }
-};
-
+async function getStateInfo(){
+    try{
+        const response = await axiosInstance.get(
+            `/weather/api/get/maininfo/`,{}
+        )
+        return response.data;
+    } catch (error) {
+        console.log('오류 발생', error);
+    }
+}
 export default getStateInfo;
